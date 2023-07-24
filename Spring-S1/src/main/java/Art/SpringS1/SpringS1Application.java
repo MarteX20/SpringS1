60355d22b1b14e3b5546b74db5999a6559d8e6d7
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-07-24 16:24:18 +0200

    Add files via upload

---
+++ Spring-S1/src/main/java/Art/SpringS1/SpringS1Application.java
@@ -0,0 +1,13 @@
+package Art.SpringS1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringS1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringS1Application.class, args);
+	}
+
+}
---
+++ Spring-S1/src/test/java/Art/SpringS1/SpringS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Art.SpringS1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
